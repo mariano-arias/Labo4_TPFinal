a45af80c2a75a875b52fea48e39e2c5636aac07f
mariano-arias <marianoluisarias@gmail.com>
2022-05-27 19:15:11 -0300

    fist commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDgQsDS0Gj_Fz1gnLFNmHShP8cljhifVVQ",
+    authDomain: "mariano-arias-tpfinal.firebaseapp.com",
+    projectId: "mariano-arias-tpfinal",
+    storageBucket: "mariano-arias-tpfinal.appspot.com",
+    messagingSenderId: "1024551720395",
+    appId: "1:1024551720395:web:2c2c2d9b03c9b41e6c924e"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDgQsDS0Gj_Fz1gnLFNmHShP8cljhifVVQ",
+    authDomain: "mariano-arias-tpfinal.firebaseapp.com",
+    projectId: "mariano-arias-tpfinal",
+    storageBucket: "mariano-arias-tpfinal.appspot.com",
+    messagingSenderId: "1024551720395",
+    appId: "1:1024551720395:web:2c2c2d9b03c9b41e6c924e"
+  }
 };
 
 /*
