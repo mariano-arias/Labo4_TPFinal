762825ed45cffe7e8220b44c41676f20b90519d7
Mariano Arias <marianoluisarias@gmail.com>
2022-06-04 01:45:44 -0300

    muestro boton activacion usuario en tabla y realizo activacion Ok

--- src/app/components/usuario-detalle/usuario-detalle.component.ts
+++ src/app/components/usuario-detalle/usuario-detalle.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Usuario } from 'src/app/Entities/usuario';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { InteractionService } from 'src/app/services/interaction.service';
 import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
@@ -11,16 +13,31 @@ export class UsuarioDetalleComponent implements OnInit {
 
   @Input() usuario : Usuario | undefined;
   
-
-  constructor(private storageService : StorageService) { 
+  userUpdated : Usuario | undefined;
+  constructor(private storageService : StorageService, private firebaseService : FirebaseService,
+              private interactionService: InteractionService) { 
     
   }
 
   ngOnInit(): void {
-   console.log("usuario seleccionado: ",this.usuario?.uid);
-   
-
   }
 
-  
+  ActivarUsuario(){
+  console.log(this.usuario);
+ 
+    this.GuardarCambios()
+  }
+
+  GuardarCambios( ){
+// console.log(this.usuario);
+
+//     this.firebaseService.Update<Usuario>(u.uid, u).then(
+//     (res)=>{
+//     console.log(res);
+//     })
+//     .catch(
+//       ()=> this.interactionService.showError("Ha ocurrido un error", "Error")
+//     )
+//   }
+  }
 }
